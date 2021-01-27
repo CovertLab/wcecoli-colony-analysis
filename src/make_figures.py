712a2f6436b6859db3b2784e8abb870ae3177388
cs <cs.temporary@icloud.com>
2021-01-27 00:22:11 +0000

    Set ridgeline overlap to put space between distributions

--- src/make_figures.py
+++ src/make_figures.py
@@ -139,7 +139,7 @@ def make_expression_distributions_fig(replicates_raw_data):
     fig = get_ridgeline_plot(
         replicates_data,
         point_alpha=1,
-        overlap=0,
+        overlap=-0.1,
         num_bins=100,
         x_label='Protein Concentration (counts/fL)',
         y_label='Distribution Density')
