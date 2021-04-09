d1f7ddf26e72d1e0084dc3e0103ba4d0320fc0ef
cs <cs.temporary@icloud.com>
2021-04-09 12:53:17 +0000

    Make matplotlib use Arial

--- src/make_figures.py
+++ src/make_figures.py
@@ -11,6 +11,7 @@ import sys
 import subprocess
 from typing import Sequence, List, Dict, Tuple
 
+from matplotlib import rcParams
 import numpy as np
 from vivarium.core.process import serialize_value
 from vivarium.core.experiment import get_in
@@ -469,6 +470,8 @@ def make_phylogeny_plot(data):
 
 def main():
     '''Generate all figures.'''
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = 'Arial'
     if not os.path.exists(FIG_OUT_DIR):
         os.makedirs(FIG_OUT_DIR)
     with open(os.path.join(FIG_OUT_DIR, METADATA_FILE), 'w') as f:
