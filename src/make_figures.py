d60acfbe55428354da50c3b34bc1515c92752725
cs <cs.temporary@icloud.com>
2021-04-16 19:54:29 +0000

    Update metadata to handle running on CI

--- src/make_figures.py
+++ src/make_figures.py
@@ -188,7 +188,22 @@ def exec_shell(
 
 def get_metadata() -> dict:
     '''Get information on which experiments and code were used.'''
-    metadata = {
+    if os.environ['CI']:
+        ci_url = '{}/{}/actions/runs/{}'.format(
+            os.environ['GITHUB_SERVER_URL'],
+            os.environ['GITHUB_REPOSITORY'],
+            os.environ['GITHUB_RUN_ID'],
+        )
+        return {
+            'git_hash': os.environ['GITHUB_SHA'],
+            'git_branch': os.environ.get('GITHUB_BASE_REF'),
+            'git_status': 'n/a because on CI',
+            'time': datetime.utcnow().isoformat() + '+00:00',
+            'python': sys.version.splitlines()[0],
+            'experiment_ids': EXPERIMENT_IDS,
+            'ci_url': ci_url,
+        }
+    return {
         'git_hash': exec_shell(['git', 'rev-parse', 'HEAD'])[0],
         'git_branch': exec_shell(
             ['git', 'symbolic-ref', '--short', 'HEAD'])[0],
@@ -198,7 +213,6 @@ def get_metadata() -> dict:
         'python': sys.version.splitlines()[0],
         'experiment_ids': EXPERIMENT_IDS,
     }
-    return metadata
 
 
 def get_experiment_ids(
