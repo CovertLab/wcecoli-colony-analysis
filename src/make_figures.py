e33c61b9321fc92c6c3343321950437aeb703564
cs <cs.temporary@icloud.com>
2021-04-16 23:39:09 +0000

    Try defining font family before setting it

--- src/make_figures.py
+++ src/make_figures.py
@@ -764,8 +764,8 @@ FIGURE_FUNCTION_MAP = {
 
 def main() -> None:
     '''Generate all figures.'''
-    rcParams['font.family'] = ['sans-serif']
     rcParams['font.sans-serif'] = ['Arial']
+    rcParams['font.family'] = ['sans-serif']
     if not os.path.exists(FIG_OUT_DIR):
         os.makedirs(FIG_OUT_DIR)
     with open(os.path.join(FIG_OUT_DIR, METADATA_FILE), 'w') as f:
