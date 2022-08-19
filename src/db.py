c80266305aebbe18febe7d07d5b4f395db53b9fa
cs <cs.temporary@icloud.com>
2022-08-19 20:57:05 +0000

    Support experiments split across multiple sims

--- src/db.py
+++ src/db.py
@@ -1,6 +1,8 @@
 import argparse
+import copy
 import json
 import os
+from typing import Union, Optional, cast
 
 from vivarium.core.emitter import (
     get_local_client,
@@ -12,9 +14,32 @@ from vivarium.library.units import remove_units
 from src.types import RawData, EnvironmentConfig, DataTuple
 
 
+class SplitExperimentSpec:
+
+    def __init__(self, spec: dict):
+        self._spec = copy.deepcopy(spec)
+
+    def items(self):
+        return self._spec.items()
+
+    def key(self):
+        return tuple(self.items())
+
+    def to_dict(self):
+        return copy.deepcopy(self._spec)
+
+    def __hash__(self):
+        return hash(self.key())
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.key() == other.key()
+
+
 def get_experiment_data(
         args: argparse.Namespace,
-        experiment_id: str,
+        experiment_id_spec: Union[str, SplitExperimentSpec],
         ) -> DataTuple:
     '''Get simulation data for an experiment.
 
@@ -24,28 +49,64 @@ def get_experiment_data(
 
     Args:
         args: Parsed CLI args.
-        experiment_id: ID of experiment.
+        experiment_id_spec: Specification for the ID(s) of the
+            experiment(s) to retrieve data for. If a string, it should
+            be the experiment ID. If a dictionary, it should be a
+            mapping from start times to experiment IDs (strings).
 
-    Returns: Tuple of simulation data and environment config.
+    Returns: Tuple of simulation data and environment config. If
+        ``experiment_id_spec`` was a dictionary, only the first
+        environment config will be returned, and the simulation data
+        will contain data from all the experiments, with each time
+        increased by the experiment's start time.
+
+    Raises:
+        AssertionError: If there are overlapping times between
+            experiments or if there is no environment config.
     '''
+    if isinstance(experiment_id_spec, str):
+        experiment_ids = SplitExperimentSpec({
+            0.0: experiment_id_spec,
+        })
+    else:
+        experiment_ids = cast(SplitExperimentSpec, experiment_id_spec)
+
+    environment_config: Optional[EnvironmentConfig] = None
+    combined_data: dict = {}
     if args.data_path:
-        path = os.path.join(
-            args.data_path, '{}.json'.format(experiment_id))
-        with open(path, 'r') as f:
-            loaded_file = json.load(f)
+        for start_time, experiment_id in experiment_ids.items():
+            path = os.path.join(
+                args.data_path, '{}.json'.format(experiment_id))
+            with open(path, 'r') as f:
+                loaded_file = json.load(f)
+                data = RawData({
+                    float(time) + start_time: value
+                    for time, value in loaded_file['data'].items()
+                })
+                if environment_config is None:
+                    environment_config = EnvironmentConfig(
+                        loaded_file['environment_config'])
+                assert not combined_data.keys() & data.keys()
+                combined_data.update(data)
+    else:
+        client = get_local_client(
+            args.host, args.port, args.database_name)
+        for start_time, experiment_id in experiment_ids.items():
+            data, _ = data_from_database(
+                experiment_id, client,
+                #filters={'data.time': {'$mod': [10, 0]}}
+            )
+            data = remove_units(deserialize_value(data))
+            if environment_config is None:
+                environment_config = data[min(data)]['dimensions']
             data = RawData({
-                float(time): value
-                for time, value in loaded_file['data'].items()
+                time + start_time: timepoint
+                for time, timepoint in data.items()
             })
-            config = EnvironmentConfig(
-                loaded_file['environment_config'])
-            return data, config
-    client = get_local_client(
-        args.host, args.port, args.database_name)
-    data, _ = data_from_database(experiment_id, client)
-    data = remove_units(deserialize_value(data))
-    environment_config = data[min(data)]['dimensions']
-    return data, environment_config
+            assert not combined_data.keys() & data.keys()
+            combined_data.update(data)
+    assert environment_config is not None
+    return RawData(combined_data), environment_config
 
 
 def add_connection_args(parser: argparse.ArgumentParser) -> None:
